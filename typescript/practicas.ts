948b3587bf33c15b916372101971df2a32997534
Rafa <rafa@Galaxy-Note9.localdomain>
2023-07-05 11:12:40 +0200

    Clase car creada con sus propiedades

--- typescript/practicas.ts
+++ typescript/practicas.ts
@@ -36,5 +36,18 @@ class Car {
   }
   
   // Metodos
-  
+    accelerate(speed: number): string {
+        return `${this.worker()} esta acelerando a una velocidad de ${speed} KM/S`
+    }
+    brake(): string{
+        return `${this.worker()} esta utilizando el sistema de frenos estandar`
+    }
+    turn(direction: 'left' | 'right'): string {
+        return `${this.worker()} is turning ${direction}`
+    }
+
+    // Esta funcion realiza el trabajo para las otras funciones del metodo.
+    worker(): string {
+        return this._make;
+    }
 }
